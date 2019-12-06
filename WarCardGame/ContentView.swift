b39d2d6e00047b15f3e5fa112cbbb49a5f0eca18
konstantinosrizos <rizos.dev@gmail.com>
2019-12-06 20:23:36 +0200

    Initial Commit

---
+++ WarCardGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WarCardGame
+//
+//  Created by Konstantinos Rizos on 6/12/19.
+//  Copyright © 2019 Konstantinos Rizos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
